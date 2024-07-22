b6a503d4d61399cdf887b405ce1724605c005512
Jorge Balderas <jorgerbf@gmail.com>
2024-07-22 14:16:38 -0400

    initial commit

---
+++ azfunction/src/main/java/com/demo/function/HttpTriggerFunction.java
@@ -0,0 +1,48 @@
+import java.util.Optional;
+
+import org.xerial.snappy.Snappy;
+
+import com.google.protobuf.InvalidProtocolBufferException;
+import com.microsoft.azure.functions.ExecutionContext;
+import com.microsoft.azure.functions.HttpMethod;
+import com.microsoft.azure.functions.HttpRequestMessage;
+import com.microsoft.azure.functions.HttpResponseMessage;
+import com.microsoft.azure.functions.HttpStatus;
+import com.microsoft.azure.functions.annotation.AuthorizationLevel;
+import com.microsoft.azure.functions.annotation.FunctionName;
+import com.microsoft.azure.functions.annotation.HttpTrigger;
+
+import prometheus.Remote.WriteRequest;
+import java.io.IOException;
+
+public class HttpTriggerFunction {
+
+    @FunctionName("HttpTriggerJava")
+    public HttpResponseMessage run(
+        @HttpTrigger(name = "req", methods = {HttpMethod.POST}, 
+            authLevel = AuthorizationLevel.FUNCTION) HttpRequestMessage<Optional<byte[]>> request,
+        final ExecutionContext context) {
+
+        context.getLogger().info("Java HTTP trigger processed a request.");
+        context.getLogger().info("Content-Type header: " + request.getHeaders().get("Content-Type"));
+
+        byte[] requestBody = request.getBody().orElse(null);
+        if (requestBody == null) {
+            return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("Invalid input").build();
+        }
+
+        try {
+            byte[] uncompressedData = Snappy.uncompress(requestBody);
+            WriteRequest writeRequest = WriteRequest.parseFrom(uncompressedData);
+            context.getLogger().info("Received prometheus request with " + writeRequest.getTimeseriesCount() + " timeseries");
+            context.getLogger().info("First timeseries has " + writeRequest.getTimeseries(0).getSamplesCount() + " samples");
+            // Process the message
+            return request.createResponseBuilder(HttpStatus.OK).body("Received message: " + writeRequest.getTimeseriesCount()).build();
+        } catch (InvalidProtocolBufferException e) {
+            return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("Failed to parse Protobuf message").build();
+        }
+        catch (IOException e) {
+            return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("Failed to uncompress Snappy message").build();
+        } 
+    }
+}
---
+++ client/src/main/java/com/demo/client/PrometheusClient.java
@@ -0,0 +1,49 @@
+import com.google.protobuf.InvalidProtocolBufferException;
+import org.xerial.snappy.Snappy;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.io.OutputStream;
+import prometheus.Remote.WriteRequest;
+import prometheus.Types.TimeSeries;
+import prometheus.Types.Sample;
+
+public class PrometheusClient {
+
+    public static void main(String[] args) throws IOException, URISyntaxException {
+        // Create a sample WriteRequest
+        WriteRequest writeRequest = WriteRequest.newBuilder()
+            .addTimeseries(
+                TimeSeries.newBuilder()
+                    .addSamples(
+                        Sample.newBuilder()
+                            .setValue(123.45)
+                            .setTimestamp(System.currentTimeMillis())
+                        .build())
+                .build())
+            .build();
+
+        // Serialize the WriteRequest to byte array
+        byte[] protobufData = writeRequest.toByteArray();
+
+        // Compress the byte array using Snappy
+        byte[] compressedData = Snappy.compress(protobufData);
+
+        // Send the compressed data to Azure Function
+        URI uri = new URI("https://<your-azure-function-url>/api/PrometheusReceiver");
+        URL url = uri.toURL();
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setDoOutput(true);
+        connection.setRequestMethod("POST");
+        connection.setRequestProperty("Content-Type", "application/x-protobuf");
+
+        try (OutputStream outputStream = connection.getOutputStream()) {
+            outputStream.write(compressedData);
+        }
+
+        int responseCode = connection.getResponseCode();
+        System.out.println("Response Code: " + responseCode);
+    }
+}
